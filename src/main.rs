f78ca3ebfc3f0db5a7c541b45ecca0a7bb1be3bd
Tar3th <001sujalkhatiwada@gmail.com>
2024-04-04 08:21:06 +0545

    Added bevy window.

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,23 @@
+use bevy::prelude::*;
 fn main() {
+    App::new()
+    .add_plugins(DefaultPlugins.set(WindowPlugin{
+        primary_window: Some(Window {
+            title:"Flappy Bevy".to_string(),
+            resolution: (400., 700.).into(),
+            resizable:false,
+            ..default()
+        }),
+        ..default()
+    }))
+    .add_systems(Startup, setup)
+    .run();
+    
+}
+
+fn setup(
+    mut commands: Commands
+){
+    commands.spawn(Camera2dBundle::default());
     println!("Hello, world!");
 }
